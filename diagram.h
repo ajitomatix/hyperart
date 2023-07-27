bbce15360d4295bf891c9ac825df54fb9fe3efd1
Ajit Datar <ajitdatar@gmail.com>
2023-07-27 12:58:40 -0700

    Added LICENCE (GPLv3) and related comment headers

--- diagram.h
+++ diagram.h
@@ -1,11 +1,15 @@
 /***************************************************************************
- *   Copyright (C) 2005 by Ajit Datar, Dr. Douglas Dunham   *
- *   ajitdatar@gmail.com, ddunham@d.umn.edu   *
+ *   HyperArt Copyright (C) 2005-2023 by Ajit Datar (ajitdatar@gmail.com)  *
+ *                                                                         *
+ *   Hyperbolic geometry drawing algorithms developed by                   *
+ *   and credited to Dr. Douglas Dunham (ddunham@d.umn.edu)                *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
+ *   the Free Software Foundation; either version 3 of the License, or     *
  *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   Please see LICENSE file for more details.                             *
  *                                                                         *
  *   This program is distributed in the hope that it will be useful,       *
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
@@ -17,6 +21,7 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
+
 #ifndef DIAGRAM_H
 #define DIAGRAM_H
 
--- parameters.h
+++ parameters.h
@@ -1,12 +1,27 @@
 /***************************************************************************
- *   Copyright (C) 2005 by Dr. Douglas Dunham, Ajit Datar                  *
- *   ddunham@d.umn.edu , data0003@d.umn.edu                                *
+ *   HyperArt Copyright (C) 2005-2023 by Ajit Datar (ajitdatar@gmail.com)  *
+ *                                                                         *
+ *   Hyperbolic geometry drawing algorithms developed by                   *
+ *   and credited to Dr. Douglas Dunham (ddunham@d.umn.edu)                *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   Please see LICENSE file for more details.                             *
  *                                                                         *
  *   This program is distributed in the hope that it will be useful,       *
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                  *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
  *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
+
 #ifndef PARAMETERS_H
 #define PARAMETERS_H
 
